5da00d031e8f3f2756d439fd6ca485a3cb309722
P3dro-Lucas <90054471+P3dro-Lucas@users.noreply.github.com>
2025-09-01 12:49:43 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+#Gerenciador de Tarefas no Terminal
+# Deve permitir o usuario Adicionar; Listar, Concluir e Salvar tarefas
+
+    #Adicionar: O Usuario escrevera a descrição.
+    #Listar: Mostra todas as tarefas numeradas.
+    #Concluir: Marca uma tarefa como feita.
+    #Salvar tarefas em um arquivo: As tarefas ficaram registradas em "tarefas.txt"
+    #Carregar tarefas ao iniciar.
+
